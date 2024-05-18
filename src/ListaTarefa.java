4d03a2e4131eafd5c2a5929faa8d0492a55bcca5
keuwey <keuwey@protonmail.com>
2024-05-18 16:06:08 -0300

    feat: add Tarefa and ListaTarefa classes

---
+++ src/ListaTarefa.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+	private List<Tarefa> tarefaList;
+
+	public ListaTarefa() {
+		this.tarefaList = new ArrayList<>();
+	}
+
+	public void adicionarTarefa(String descricao) {
+		this.tarefaList.add(new Tarefa(descricao));
+	}
+
+	public void removerTarefa(String descricao) {
+		List<Tarefa> tarefasParaRemover = new ArrayList<>();
+		for (int i = 0; i < this.tarefaList.size(); i++) {
+			if (this.tarefaList.get(i).getDescricao().equalsIgnoreCase(descricao)) {
+				tarefasParaRemover.add(this.tarefaList.get(i));
+			}
+		}
+		tarefaList.removeAll(tarefasParaRemover);
+	}
+
+	public int obterNumeroTotalTarefas() {
+		return this.tarefaList.size();
+	}
+
+	public void obterDescricaoTarefa() {
+		System.out.println(this.tarefaList);
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
